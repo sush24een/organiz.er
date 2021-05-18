4ab20263d959ebbd4a88f3aa18127263dd16098b
Susheen Kanungo <sheenukanungo@gmail.com>
2021-05-19 00:28:47 +0530

    added footer and worked on study with me

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
