795f3033c4815f7967437fe14cc5785436563f32
Vip-Bhardw <vipulbhardwaj1011@gmail.com>
2021-05-19 12:39:35 +0530

    PT done except Mongo backend

--- my-app/src/components/ProductivityTracker.js
+++ my-app/src/components/ProductivityTracker.js
@@ -1,37 +1,135 @@
 import React from 'react';
 import "./ProductivityTracker.css";
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    Card, CardBody, CardTitle, CardSubtitle, Alert,
+    Button, Form, FormGroup, Label, Input
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProductivityTracker() {
+    const [timerText, setTimerText] = React.useState("Start Timer");
+    const [timerState, setTimerState] = React.useState(false);
+    const [elapsedTime, setTimeElapsed] = React.useState();
+    const[startTime, setStartTime] = React.useState();
+
+    function showElapsedTime() {
+        if(timerState) {
+            setTimeElapsed(new Date().getTime() - startTime);
+        }
+    }
+
+    function changeTimerState() {
+        if(timerState) {
+            setTimerState(false);
+            setTimerText("Start Timer");
+            setTimeElapsed(new Date().getTime() - startTime);
+        } else {
+            setTimerState(true);
+            setTimerText("End Timer");
+            setStartTime(new Date().getTime());
+        }//console.log(timerState, timerText);
+    }
+
+    function submitTime() {
+        if(timerState) {
+
+        }
+    }
+
     return (
-        <div>
+        <div className="productivityComponent">
             <h1 className="rainbow-text-PT">Productivity Tracker</h1>
             <hr/>
-            <div className="manual">
-                <Card>
-                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                    <CardBody>
-                        <CardTitle tag="h5">Manual</CardTitle>
-                        <CardText>Enter the time intervals for specific major tasks you spent your time on, for today.</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
+            <div>
+                <div className="manual">
+                    <Card style={{ padding: '10px', backgroundColor: '#7000da', borderColor: 'black' }}>
+                        <CardBody>
+                            <CardTitle tag="h5">Manual</CardTitle>
+                            <CardSubtitle tag="h6">Enter the time intervals for specific major tasks you spent your time on, for today.</CardSubtitle>
+                            <Alert style={{ marginTop: '10px', color:'#7000da', backgroundColor: '#333', borderColor: '#333' }}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="exampleSelect">Task Done</Label>
+                                        <Input type="select" name="select" id="exampleSelect">
+                                        <option>-- select --</option>
+                                        <option>Studied</option>
+                                        <option>Exercised / Yoga</option>
+                                        <option>Worked</option>
+                                        <option>Daily Routines</option>
+                                        <option>Played</option>
+                                        <option>Slept</option>
+                                        <option>Misc.</option>
+                                        </Input>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="exampleEmail">Description (optional) :</Label>
+                                        <Input type="textarea" name="text" id="exampleText"/>
+                                    </FormGroup>
+                                    <span>
+                                        <FormGroup>
+                                            <label for="appt">FROM -&nbsp;&nbsp;</label>
+                                            <input type="time" id="appt" name="appt" required />
+                                            <label for="appt">&nbsp;&nbsp;, TO -&nbsp;&nbsp;</label>
+                                            <input type="time" id="appt" name="appt" required />
+                                        </FormGroup>
+                                    </span>
+                                </Form>
+                            </Alert>
+                            <Button style={{ float:'right', color:'#7000da', backgroundColor: '#333', borderColor: '#333' }}>&#10004;</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="counter">
+                    <Card style={{ padding: '10px', backgroundColor: '#7000da', borderColor: 'black' }}>
+                        <CardBody>
+                            <CardTitle tag="h5">Counted</CardTitle>
+                            <CardSubtitle tag="h6">Enter the Task Name/Activity Name and start the timer and let the time be counted by itself.</CardSubtitle>
+                            <Alert style={{ marginTop: '10px', color:'#7000da', backgroundColor: '#333', borderColor: '#333' }}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="exampleSelect">Task Done</Label>
+                                        <Input type="select" name="select" id="exampleSelect">
+                                        <option>-- select --</option>
+                                        <option>Studying</option>
+                                        <option>Exercised / Yoga</option>
+                                        <option>Worked</option>
+                                        <option>Daily Routines</option>
+                                        <option>Played</option>
+                                        <option>Slept</option>
+                                        <option>Misc.</option>
+                                        </Input>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="exampleEmail">Description (optional) :</Label>
+                                        <Input type="textarea" name="text" id="exampleText"/>
+                                    </FormGroup>
+                                </Form>
+                                <div>
+                                    <Button onClick={ changeTimerState } style={{ backgroundColor: '#7000da', borderColor: 'black' }}>
+                                        { timerText }
+                                    </Button>
+                                    &nbsp;&nbsp;Started At :&nbsp;&nbsp;
+                                    <span style={{fontWeight: 800, fontSize: '30px'}}>{ startTime }</span>
+                                </div>
+                                <div>
+                                    <Button onClick={ showElapsedTime } style={{ backgroundColor: '#7000da', borderColor: 'black' }}>
+                                        Show Elapsed Time
+                                    </Button>
+                                    &nbsp;&nbsp;Time Elapsed :&nbsp;&nbsp;
+                                    <span style={{fontWeight: 800, fontSize: '30px'}}>{ elapsedTime }</span>
+                                </div>
+                            </Alert>
+                            <Button onClick={ submitTime } style={{ float:'right', color:'#7000da', backgroundColor: '#333', borderColor: '#333' }}>&#10004;</Button>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
-            <div className="counter">
-                <Card>
-                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                    <CardBody>
-                        <CardTitle tag="h5">Counted</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                        <CardText>Enter the Task Name/Activity Name and start the timer and let the time be counted by itself.</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
-            </div>
+            {/*<div className="AcessPT">
+                <Button body inverse style={{ padding: '10px', color:'#white', backgroundColor: '#7000da', borderColor: '#333' }}>
+                    Access Your Time Management So Far
+                </Button>
+            </div>*/
+            }
         </div>
     );
 }
